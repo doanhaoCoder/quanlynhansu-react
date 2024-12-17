8e6724e4075f1a7cb8145dea28cd334c441dceca
doanhaoCoder <doannhathao2310@gmail.com>
2024-12-18 01:52:11 +0700

    update images

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -295,10 +295,10 @@ const Dashboard = () => {
             <div class="logo-header" data-background-color="dark">
               <a href="index.html" class="logo">
                 <img
-                  src="assets/img/kaiadmin/logo_light.svg"
+                  src="https://logos.textgiraffe.com/logos/logo-name/Admin-designstyle-colors-m.png"
                   alt="navbar brand"
                   class="navbar-brand"
-                  height="20"
+                  height="125"
                 />
               </a>
               <div class="nav-toggle">
