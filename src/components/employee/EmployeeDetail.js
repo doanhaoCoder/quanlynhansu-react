ed1bb251af8e1b5c664091ea512cbc1d88104428
doanhaoCoder <doannhathao2310@gmail.com>
2024-12-18 22:42:40 +0700

    update

--- src/components/employee/EmployeeDetail.js
+++ src/components/employee/EmployeeDetail.js
@@ -14,8 +14,38 @@ const EmployeeDetail = () => {
       try {
         const employeeRef = doc(db, "nhanvien", id); // Dùng ID để lấy tài liệu
         const employeeSnap = await getDoc(employeeRef);
+        
         if (employeeSnap.exists()) {
-          setEmployee(employeeSnap.data());
+          const employeeData = employeeSnap.data();
+          // console.log("Nhân viên:", employeeData);
+
+          if (employeeData.PhongBan) {
+            const phongRef = doc(db, "phongban", employeeData.PhongBan);
+            const phongSnap = await getDoc(phongRef);
+            if (phongSnap.exists()) {
+              const phongData = phongSnap.data();
+              employeeData.tenPhong = phongData.tenPhong; // Kiểm tra tên trường tại đây
+              console.log("check: ", employeeData.tenPhong);
+            }
+          }
+          
+          if (employeeData.ChucVu) {
+            const chucVuRef = doc(db, "chucvu", employeeData.ChucVu);
+            const chucVuSnap = await getDoc(chucVuRef);
+            if (chucVuSnap.exists()) {
+              const chucVuData = chucVuSnap.data();
+              employeeData.tenChucVu = chucVuData.tenChucVu; // Kiểm tra tên trường tại đây
+              employeeData.luongChucVu = chucVuData.luong; // Kiểm tra tên trường tại đây
+            }
+          }
+
+          // Cập nhật state employee với dữ liệu đầy đủ
+          setEmployee(employeeData);
+          console.log("Dữ liệu nhân viên sau khi thêm TenPhong và TenChucVu:", employeeData);
+        }
+
+        if (employeeSnap.exists()) {
+          // setEmployee(employeeSnap.data());
         } else {
           console.error("No such document!");
         }
@@ -135,13 +165,13 @@ const EmployeeDetail = () => {
               <strong>Ghi chú:</strong> {employee.GhiChu}
             </p>
             <p>
-              <strong>Phòng ban:</strong> {employee.MaThueTNCN}
+              <strong>Phòng ban:</strong> {employee.tenPhong}
             </p>
             <p>
-              <strong>Chức vụ:</strong> {employee.MaThueTNCN}
+              <strong>Chức vụ:</strong> {employee.tenChucVu}
             </p>
             <p>
-              <strong>Lương:</strong> {employee.MaThueTNCN}
+              <strong>Lương:</strong> {employee.luongChucVu} VNĐ
             </p>
           </div>
         </div>
