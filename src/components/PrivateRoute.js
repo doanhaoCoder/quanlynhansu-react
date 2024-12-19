19101b8176fe354c69df1890aad9cadd0e25c07e
doanhaoCoder <doannhathao2310@gmail.com>
2024-12-19 08:14:22 +0700

    u

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,12 +4,14 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
   const user = JSON.parse(sessionStorage.getItem("user"));
 
-  if (!user) {
-    // Nếu chưa đăng nhập, chuyển hướng đến trang đăng nhập
+  // Kiểm tra nếu người dùng chưa đăng nhập hoặc role không phải là "admin" hoặc "nhân viên"
+  if (!user || (user.role !== "Quản trị viên" && user.role !== "Nhân viên")) {
+    // Nếu chưa đăng nhập hoặc không có quyền truy cập, chuyển hướng đến trang đăng nhập
+    alert("Tài khoản nầy không có quyền truy cập. hãy sử dụng tài khoản khác!");
     return <Navigate to="/dang-nhap" />;
   }
 
-  // Nếu đã đăng nhập, hiển thị nội dung con
+  // Nếu đã đăng nhập và có quyền truy cập, hiển thị nội dung con
   return children;
 };
 
