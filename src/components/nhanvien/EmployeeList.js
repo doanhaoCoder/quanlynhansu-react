e675da407f2ea5754dc848c71b9590ba92779026
doanhaoCoder <doannhathao2310@gmail.com>
2024-12-25 23:18:36 +0700

    Thêm chức năng xuất danh sách nhân viên ra file Excel và cải thiện việc lấy thông tin phòng ban, chức vụ từ cơ sở dữ liệu

--- src/components/nhanvien/EmployeeList.js
+++ src/components/nhanvien/EmployeeList.js
@@ -1,29 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 import { toast } from "react-toastify";
-// import {
-//   FaEye,
-//   FaEdit,
-//   FaTrashAlt,
-//   FaSort,
-//   FaSortUp,
-//   FaSortDown,
-// } from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { FaEye, FaEdit, FaTrashAlt } from "react-icons/fa";
-import { FaSort } from "react-icons/fa";
-
-
+import { FaEye, FaEdit, FaTrashAlt, FaSort } from "react-icons/fa";
+import ExcelJS from "exceljs";
+import { saveAs } from "file-saver";
 
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [sortColumn, setSortColumn] = useState("");
-  const [sortOrder, setSortOrder] = useState("asc"); // "asc" hoặc "desc"
+  const [sortOrder, setSortOrder] = useState("asc");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -32,10 +23,31 @@ const EmployeeList = () => {
       try {
         const employeeRef = collection(db, "nhanvien");
         const querySnapshot = await getDocs(employeeRef);
-        const employeesData = [];
-        querySnapshot.forEach((doc) => {
-          employeesData.push({ ...doc.data(), id: doc.id });
-        });
+        const employeesData = await Promise.all(
+          querySnapshot.docs.map(async (docSnapshot) => {
+            const employee = { ...docSnapshot.data(), id: docSnapshot.id };
+
+            if (employee.PhongBan) {
+              const phongBanRef = doc(db, "phongban", employee.PhongBan);
+              const phongBanSnap = await getDoc(phongBanRef);
+              if (phongBanSnap.exists()) {
+                employee.tenPhong = phongBanSnap.data().tenPhong;
+              }
+            }
+
+            if (employee.ChucVu) {
+              const chucVuRef = doc(db, "chucvu", employee.ChucVu);
+              const chucVuSnap = await getDoc(chucVuRef);
+              if (chucVuSnap.exists()) {
+                const chucVuData = chucVuSnap.data();
+                employee.tenChucVu = chucVuData.tenChucVu;
+                employee.luongChucVu = chucVuData.luong;
+              }
+            }
+
+            return employee;
+          })
+        );
         setEmployees(employeesData);
       } catch (error) {
         console.error("Error fetching employees: ", error);
@@ -65,10 +77,8 @@ const EmployeeList = () => {
   // Hàm xử lý sắp xếp
   const handleSort = (column) => {
     if (sortColumn === column) {
-      // Nếu cột hiện tại đã được sắp xếp, chỉ cần thay đổi thứ tự
       setSortOrder(sortOrder === "asc" ? "desc" : "asc");
     } else {
-      // Nếu chọn cột mới, thiết lập cột và thứ tự mặc định là "asc"
       setSortColumn(column);
       setSortOrder("asc");
     }
@@ -76,7 +86,7 @@ const EmployeeList = () => {
 
   // Hàm sắp xếp danh sách nhân viên
   const sortedEmployees = [...employees].sort((a, b) => {
-    if (!sortColumn) return 0; // Không sắp xếp nếu chưa chọn cột
+    if (!sortColumn) return 0;
     if (sortOrder === "asc") {
       return a[sortColumn]?.localeCompare?.(b[sortColumn]) || 0;
     } else {
@@ -95,15 +105,94 @@ const EmployeeList = () => {
   // Hàm hiển thị icon sắp xếp
   const renderSortIcon = (column) => {
     if (sortColumn === column) {
-      return sortOrder === "asc" ? <FontAwesomeIcon icon={faArrowUp} /> :  <FontAwesomeIcon icon={faArrowDown} />
+      return sortOrder === "asc" ? <FontAwesomeIcon icon={faArrowUp} /> : <FontAwesomeIcon icon={faArrowDown} />;
     }
     return <FaSort />;
   };
 
+  // Hàm xuất dữ liệu ra Excel
+  const exportToExcel = async () => {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet("Danh Sách Nhân Viên");
+
+    // Thêm tiêu đề cột
+    worksheet.columns = [
+      { header: "STT", key: "stt", width: 10 },
+      { header: "Mã NV", key: "MaNV", width: 20 },
+      { header: "Họ Tên NV", key: "HoTenNV", width: 30 },
+      { header: "Email", key: "Email", width: 30 },
+      { header: "Số Điện Thoại", key: "SDT", width: 20 },
+      { header: "Ngày Sinh", key: "NgaySinh", width: 20 },
+      { header: "Giới Tính", key: "GioiTinh", width: 10 },
+      { header: "CCCD", key: "CCCD", width: 20 },
+      { header: "Ngày Cấp", key: "NgayCap", width: 20 },
+      { header: "Nơi Cấp", key: "NoiCap", width: 20 },
+      { header: "Dân Tộc", key: "DanToc", width: 20 },
+      { header: "Tôn Giáo", key: "TonGiao", width: 20 },
+      { header: "Địa Chỉ Tạm Trú", key: "DCTamTru", width: 30 },
+      { header: "Địa Chỉ Thường Trú", key: "DCThuongTru", width: 30 },
+      { header: "Nơi Sinh", key: "NoiSinh", width: 20 },
+      { header: "Trình Độ Học Vấn", key: "TrinhDoHocVan", width: 20 },
+      { header: "Trình Độ Chuyên Môn", key: "TrinhDoChuyenMon", width: 20 },
+      { header: "Tình Trạng Hôn Nhân", key: "TinhTrangHonNhan", width: 20 },
+      { header: "Mã Thuế TNCN", key: "MaThueTNCN", width: 20 },
+      { header: "Mã BH", key: "MaBH", width: 20 },
+      { header: "Tình Trạng", key: "TinhTrang", width: 20 },
+      { header: "Ghi Chú", key: "GhiChu", width: 30 },
+      { header: "Phòng Ban", key: "tenPhong", width: 20 },
+      { header: "Chức Vụ", key: "tenChucVu", width: 20 },
+      { header: "Lương Ngày", key: "luongChucVu", width: 20 },
+    ];
+
+    // Thêm dữ liệu vào worksheet
+    filteredEmployees.forEach((employee, index) => {
+      worksheet.addRow({
+        stt: index + 1,
+        MaNV: employee.MaNV,
+        HoTenNV: employee.HoTenNV,
+        Email: employee.Email,
+        SDT: employee.SDT,
+        NgaySinh: employee.NgaySinh,
+        GioiTinh: employee.GioiTinh,
+        CCCD: employee.CCCD,
+        NgayCap: employee.NgayCap,
+        NoiCap: employee.NoiCap,
+        DanToc: employee.DanToc,
+        TonGiao: employee.TonGiao,
+        DCTamTru: employee.DCTamTru,
+        DCThuongTru: employee.DCThuongTru,
+        NoiSinh: employee.NoiSinh,
+        TrinhDoHocVan: employee.TrinhDoHocVan,
+        TrinhDoChuyenMon: employee.TrinhDoChuyenMon,
+        TinhTrangHonNhan: employee.TinhTrangHonNhan,
+        MaThueTNCN: employee.MaThueTNCN,
+        MaBH: employee.MaBH,
+        TinhTrang: employee.TinhTrang,
+        GhiChu: employee.GhiChu,
+        tenPhong: employee.tenPhong,
+        tenChucVu: employee.tenChucVu,
+        luongChucVu: employee.luongChucVu.toLocaleString() + " VNĐ",
+      });
+    });
+
+    // Xuất file Excel
+    const buffer = await workbook.xlsx.writeBuffer();
+    const blob = new Blob([buffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    saveAs(blob, "DanhSachNhanVien.xlsx");
+  };
+
   return (
     <div className="mt-5">
-      <h2>Danh Sách Nhân Viên 
-        <a href="/dashboard/them-nhan-vien" className="btn btn-primary mb-2 ms-2">Thêm Nhân Viên</a>
+      <h2>
+        Danh Sách Nhân Viên
+        <a href="/dashboard/them-nhan-vien" className="btn btn-primary mb-2 ms-2">
+          Thêm Nhân Viên
+        </a>
+        <button className="btn btn-success mb-2 ms-2" onClick={exportToExcel}>
+          Xuất Excel
+        </button>
       </h2>
       <div className="row mt-4">
         <div className="col-6">
